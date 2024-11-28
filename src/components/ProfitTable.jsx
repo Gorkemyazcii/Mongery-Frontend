6dfe2348be3991dcbf4a83548201c8a72bc22422
Görkem <123131846+Gorkemyazcii@users.noreply.github.com>
2024-11-28 22:40:52 +0300

    Update ProfitTable.jsx

--- src/components/ProfitTable.jsx
+++ src/components/ProfitTable.jsx
@@ -1,11 +1,8 @@
-
-import React from 'react';
-import { useGetOrdersQuery } from '../redux/apiSlice'; 
+import React from "react";
+import { useGetOrdersQuery } from "../redux/apiSlice";
 
 function ProfitTable() {
   const { data, error, isLoading } = useGetOrdersQuery();
-  console.log(data);
-  
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -15,69 +12,31 @@ function ProfitTable() {
     <div>
       <h1>Orders</h1>
       <div>
-       <table className="w-full border-collapse border border-gray-300">
-         <thead className="bg-gray-100">
-           <tr>
-             <th>Müşteri</th>
-             <th>Fatura Numarası</th>
-             <th>Toplam Miktar</th>
-             <th>Toplam Tutar</th>
-             <th>Toplam Maliyet</th>
-             <th>Toplam Karlılık</th>
-             <th>Net Kar</th>
-           </tr>
-         </thead>
-         <tbody>
-           {data.map((row, index) => (
-            <tr key={index}>
-              <td>{JSON.parse(row.products).reduce.companyname}</td>
-              <td>{row.invoice_number}</td>
-              <td>{row.products[0].quantity} ton</td>
-              <td>{(row.subtotal * row.primary_rate).toFixed(2)} USD</td>
-              <td>
-                {row.products
-                  .reduce((total, product) => {
-                    return (
-                      total +
-                      product.stocklogs.reduce(
-                        (sum, log) =>
-                          sum +
-                          log.stock_cost +
-                          log.shipment_cost +
-                          log.credit_cost,
-                        0
-                      )
-                    );
-                  }, 0)
-                  .toFixed(2)}{" "}
-                USD
-              </td>
-              <td>
-                {(
-                  row.subtotal * row.primary_rate -
-                  row.products.reduce((total, product) => {
-                    return (
-                      total +
-                      product.stocklogs.reduce(
-                        (sum, log) =>
-                          sum +
-                          log.stock_cost +
-                          log.shipment_cost +
-                          log.credit_cost,
-                        0
-                      )
-                    );
-                  }, 0)
-                ).toFixed(2)}{" "}
-                USD
-              </td>
-              <td>
-                {(
-                  (row.subtotal * row.primary_rate -
-                    row.products.reduce((total, product) => {
+        <table className="w-full border-collapse border border-gray-300">
+          <thead className="bg-gray-100">
+            <tr>
+              <th>Müşteri</th>
+              <th>Fatura Numarası</th>
+              <th>Toplam Miktar</th>
+              <th>Toplam Tutar</th>
+              <th>Toplam Maliyet</th>
+              <th>Toplam Karlılık</th>
+              <th>Net Kar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((row, index) => (
+              <tr key={index}>
+                <td>{JSON.parse(row?.customer || null)?.companyname}</td>
+                <td>{row?.invoice_number}</td>
+                <td>{JSON.parse(row?.products || null)?.[0]?.quantity} ton</td>
+                <td>{(row?.subtotal * row?.primary_rate).toFixed(2)} USD</td>
+                <td>
+                  {JSON.parse(row?.products || null)
+                    ?.reduce((total, product) => {
                       return (
                         total +
-                        product.stocklogs.reduce(
+                        product?.stocklogs?.reduce(
                           (sum, log) =>
                             sum +
                             log.stock_cost +
@@ -86,16 +45,60 @@ function ProfitTable() {
                           0
                         )
                       );
-                    }, 0)) *
-                  0.5
-                ).toFixed(2)}{" "}
-                USD
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                    }, 0)
+                    ?.toFixed(2)}{" "}
+                  USD
+                </td>
+                <td>
+                  {(
+                    row?.subtotal * row?.primary_rate -
+                    JSON.parse(row?.products || null)?.reduce(
+                      (total, product) => {
+                        return (
+                          total +
+                          product?.stocklogs?.reduce(
+                            (sum, log) =>
+                              sum +
+                              log.stock_cost +
+                              log.shipment_cost +
+                              log.credit_cost,
+                            0
+                          )
+                        );
+                      },
+                      0
+                    )
+                  )?.toFixed(2)}{" "}
+                  USD
+                </td>
+                <td>
+                  {(
+                    (row?.subtotal * row?.primary_rate -
+                      JSON.parse(row?.products || null)?.reduce(
+                        (total, product) => {
+                          return (
+                            total +
+                            product?.stocklogs?.reduce(
+                              (sum, log) =>
+                                sum +
+                                log.stock_cost +
+                                log.shipment_cost +
+                                log.credit_cost,
+                              0
+                            )
+                          );
+                        },
+                        0
+                      )) *
+                    0.5
+                  )?.toFixed(2)}{" "}
+                  USD
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
